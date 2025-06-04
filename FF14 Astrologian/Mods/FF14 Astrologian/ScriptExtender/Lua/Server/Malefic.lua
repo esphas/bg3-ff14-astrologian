b95289df182d3999457c50247691cb07059cffea
esphas <esphas@hotmail.com>
2025-06-04 13:08:01 +0800

    save

--- FF14 Astrologian/Mods/FF14 Astrologian/ScriptExtender/Lua/Server/Malefic.lua
+++ FF14 Astrologian/Mods/FF14 Astrologian/ScriptExtender/Lua/Server/Malefic.lua
@@ -1,9 +1,21 @@
 
 local debug = false
 
-local malefics = { "MALEFIC_II", "MALEFIC_III", "MALEFIC_IV", "FALL_MALEFIC" }
-local function isMaleficTarget(status)
+local malefics = { "MaleficII", "MaleficIII", "MaleficIV", "FallMalefic" }
+local MALEFICS = { "MALEFIC_II", "MALEFIC_III", "MALEFIC_IV", "FALL_MALEFIC" }
+
+local function isMaleficSpell(spell)
     for _, malefic in ipairs(malefics) do
+        local prefix = "Target_EK_FF14_" .. malefic
+        if string.sub(spell, 1, #prefix) == prefix then
+            return true
+        end
+    end
+    return false
+end
+
+local function isMaleficTarget(status)
+    for _, malefic in ipairs(MALEFICS) do
         local prefix = "EK_FF14_" .. malefic .. "_TARGET"
         if string.sub(status, 1, #prefix) == prefix then
             return {
@@ -16,7 +28,7 @@ local function isMaleficTarget(status)
     return false
 end
 local function isMaleficDamageDealer(status)
-    for _, malefic in ipairs(malefics) do
+    for _, malefic in ipairs(MALEFICS) do
         if status == "EK_FF14_" .. malefic .. "_DAMAGE_DEALER" then
             return malefic
         end
@@ -40,21 +52,12 @@ local maleficDamageTypes = {
     { id = 13, name = "Psychic" },
 }
 
-Ext.Osiris.RegisterListener("StatusApplied", 4, "before", function(target, status, causee, _)
-    local malefic = isMaleficTarget(status)
-    if not malefic then return end
-    if debug then _P(status .. " : from " .. causee .. " to " .. target) end
-    local damageType = maleficDamageTypes[Osi.Random(#maleficDamageTypes)+1]
-    if debug then _P(malefic.damageType .. ": " .. damageType.id .. " - " .. damageType.name) end
-    Osi.SetVarFixedString(target, malefic.malefic, malefic.damageDealer);
-    Osi.ApplyStatus(target, malefic.damageType, damageType.id * 6.0, 1, causee)
-end)
-
-Ext.Osiris.RegisterListener("StatusRemoved", 4, "before", function (target, status, causee)
-    local malefic = isMaleficDamageDealer(status)
-    if not malefic then return end
-    local damageDealer = Osi.GetVarFixedString(target, malefic)
-    local prefix = "EK_FF14_" .. malefic
-    if string.sub(damageDealer, 1, #prefix) ~= prefix then return end
-    Osi.ApplyStatus(target, damageDealer, 6.0, 1, causee)
+Ext.Osiris.RegisterListener("StartedPreviewingSpell", 4, "after", function(caster, spell, _isMostPowerful, _hasMultipleLevels)
+    if not isMaleficSpell(spell) then return end
+    for _, malefic in ipairs(MALEFICS) do
+        local randomResult = maleficDamageTypes[Osi.Random(#maleficDamageTypes)+1]
+        local damageType = "EK_FF14_" .. malefic .. "_DAMAGE_TYPE"
+        if debug then _P(malefic .. ": " .. randomResult.id .. " - " .. randomResult.name) end
+        Osi.ApplyStatus(caster, damageType, randomResult.id * 6.0, 1, caster)
+    end
 end)
